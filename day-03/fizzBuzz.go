c35400f5c78909226d0d6592d45b689b081cfd58
I-Maged <magedhanafy@gmail.com>
2026-06-06 00:37:10 +0300

    day 03

---
+++ day-03/fizzBuzz.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func fizzBuzz(n int) string {
+	switch {
+	case n%15 == 0:
+		return "FizzBuzz"
+	case n%3 == 0:
+		return "Fizz"
+	case n%5 == 0:
+		return "Buzz"
+	default:
+		return fmt.Sprintf("%d", n)
+	}
+}
---
+++ day-03/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"strconv"
+)
+
+func main() {
+	if n, err := strconv.Atoi("123"); err == nil {
+		fmt.Printf("Parsed number: %d\n", n)
+	} else {
+		fmt.Printf("Error: %v\n", err)
+	}
+
+outer:
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if i == 1 && j == 1 {
+				break outer
+			}
+			fmt.Printf("i==%d, & j==%d\n", 1, j)
+		}
+	}
+
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("Mac")
+	case "linux":
+		fmt.Println("Linux")
+	default:
+		fmt.Printf("Other: %s\n", os)
+	}
+
+	names := []string{"maged", "Ibrahim"}
+	for _, name := range names {
+		fmt.Println(name)
+	}
+
+	fmt.Printf("%s\n", fizzBuzz(7))
+}
